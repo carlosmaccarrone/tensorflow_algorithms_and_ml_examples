34468d9ebdc896023b1924ffde795a4961c82b5e
Carlos <carlosmaccarrone@gmail.com>
2020-09-13 17:37:06 -0300

    clasificador binario SVM - tensorflow 1 & 2

--- clasificador_lineal_svm_tf1.py
+++ clasificador_lineal_svm_tf1.py
@@ -57,7 +57,7 @@ for _ in range(2000):
 pend = session.run(pendiente)
 ordOrigen = session.run(ordenadaAlOrigen)
 calculoSignado = lambda x, y: np.sign(np.add(np.matmul([x,y], pend), ordOrigen)).flatten()
-predicciones = [ calculoSignado(x, y)[0] for x, y in x_y_vals_test ]
+predicciones = [ calculoSignado(x, y) for x, y in x_y_vals_test ]
 precision = np.mean(np.array(np.equal(predicciones, z_vals_test), dtype=np.float32))
 print("\nPrecición de la prueba de clasificación: {}%".format(str(precision*100)))
 
