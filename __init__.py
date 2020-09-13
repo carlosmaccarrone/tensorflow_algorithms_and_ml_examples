0db572e43b8b29ac005e3b5a4adee6fc92b39dab
Carlos <carlosmaccarrone@gmail.com>
2020-09-12 22:14:59 -0300

    clasificador binario SVM - tensorflow 1 & 2

---
+++ __init__.py
@@ -0,0 +1,5 @@
+## __init__ file para el modulo de mocks de dataset de tensorflow.
+## Carlos Maccarrone
+## Python >= 3.8
+## (: :) (: :)
+## Necesario para importar conjuntos de datos desde datasetMocks.py
